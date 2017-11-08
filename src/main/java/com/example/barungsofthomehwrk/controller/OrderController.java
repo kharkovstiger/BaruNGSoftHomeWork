7c6375ccc66146b911546b72d6d3de9fd760a60f
kharkovstiger <kharkovs.tiger@gmail.com>
2017-11-08 15:53:13 +0300

    number of orders

--- src/main/java/com/example/barungsofthomehwrk/controller/OrderController.java
+++ src/main/java/com/example/barungsofthomehwrk/controller/OrderController.java
@@ -9,13 +9,16 @@ import com.example.barungsofthomehwrk.service.OrderService;
 import com.example.barungsofthomehwrk.validator.OrderValidator;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = OrderController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -33,6 +36,13 @@ public class OrderController {
     @GetMapping(value = "/getByCustomerId")
     public List<Order> getByCustomerId(@PathParam("id") String id){
         return orderService.getByCustomerId(id);
+    }
+
+    @PostMapping(value = "/getNumberOfOrders")
+    public Map<String, Integer> getNumberOfOrders(@RequestBody List<String> list){
+        Map<String, Integer> result=new HashMap<>();
+        list.forEach(id->result.put(id,orderService.getByCustomerId(id).size()));
+        return result;
     }
 
     @GetMapping(value = "/{id}")
--- src/main/java/com/example/barungsofthomehwrk/service/OrderService.java
+++ src/main/java/com/example/barungsofthomehwrk/service/OrderService.java
@@ -3,6 +3,7 @@ package com.example.barungsofthomehwrk.service;
 import com.example.barungsofthomehwrk.model.Order;
 
 import java.util.List;
+import java.util.Map;
 
 public interface OrderService {
 
